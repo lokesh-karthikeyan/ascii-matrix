e5c63c96b597c097e679ca184474b4bc00e56ebc
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-05-10 21:45:19 +0530

    Feat: Add fn() to parse cli-args

---
+++ src/ascii_matrix/cli/parser.py
@@ -0,0 +1,9 @@
+import argparse
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(description="ASCII Matrix CLI")
+
+    parser.add_argument("-i", "--image", type=str, required=True, help="Name of the image")
+    parser.add_argument("-c", "--color", type=str, default="white", choices=["red", "green", "blue"], help="Name of the color (Red, Green, Blue, White (Default))")
+
+    return parser.parse_args(args)
