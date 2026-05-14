dc9af8efc40e4218c364532fbb9d714a1b2679b5
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-05-14 22:06:37 +0530

    Feat: Add white color to the choices

--- src/ascii_matrix/cli/parser.py
+++ src/ascii_matrix/cli/parser.py
@@ -4,6 +4,6 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(description="ASCII Matrix CLI")
 
     parser.add_argument("-i", "--image", type=str, required=True, help="Name of the image")
-    parser.add_argument("-c", "--color", type=str, default="white", choices=["red", "green", "blue"], help="Name of the color (Red, Green, Blue, White (Default))")
+    parser.add_argument("-c", "--color", type=str, default="white", choices=["red", "green", "blue", "white"], help="Name of the color (Red, Green, Blue, White (Default))")
 
     return parser.parse_args(args)
