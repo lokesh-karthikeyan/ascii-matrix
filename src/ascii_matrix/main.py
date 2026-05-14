3a30d0304a25223a9a0cd18a68cacd1e00b861c9
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-05-14 22:09:25 +0530

    Feat: Add main fn() logic

--- src/ascii_matrix/main.py
+++ src/ascii_matrix/main.py
@@ -1,5 +1,17 @@
+from ascii_matrix.cli.parser import parse_args
+from ascii_matrix.fs.validate_path import validate_image_path
+from ascii_matrix.ascii_image.image import Image
+from ascii_matrix.ascii_image.to_ascii import to_ascii
+from ascii_matrix.effects.matrix import matrix
+from ascii_matrix.cli.render import render
+
 def main():
-    print('Ascii Matrix art')
+    args = parse_args()
+    image_path = validate_image_path(args.image)
+    image = Image(image_path).image
+    ascii_art = to_ascii(image)
+    frames = matrix(ascii_art, args.color)
+    render(frames)
 
 if __name__ == "__main__":
     main()
